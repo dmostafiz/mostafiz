138418af83e2d270765efc5559a92ffe577db720
mostafizrr2 <dev.mostafiz@gmail.com>
2022-08-19 20:33:45 +0600

    dynamic import

--- src/Components/ThreeD/ThreeDCarModel.jsx
+++ src/Components/ThreeD/ThreeDCarModel.jsx
@@ -6,25 +6,18 @@ import { angleToRadians } from '../../Helpers/angleToRadians'
 import { Box } from '@chakra-ui/react'
 import Three from './Three'
 import { useInView } from 'react-intersection-observer'
-import { Box2 } from 'three'
 
 export default function ThreeDCarModel() {
 
   const { ref, inView } = useInView();
 
-  // const [view, setView] = useState(null)
-
-  const view = useMemo(() => {
-
-    return inView
-
-  }, [inView])
-
   return (
 
     <Box ref={ref} height={{ base: '380px', md: '550px' }} w='full' bg='#ffd801'>
       <Canvas flat linear shadows>
-        {view && <Three />}
+        <Suspense>
+          <Three key={inView} />
+        </Suspense>
       </Canvas>
     </Box>
 
